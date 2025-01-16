fb98dc28b50deb22e8377d1f22faf0bad58389ef
FelpFS1 <phff800@gmail.com>
2025-01-16 15:52:31 -0300

    feat: add show friend list feature

--- app.js
+++ app.js
@@ -5,8 +5,20 @@ function addFriend(){
     if(friendName.value){
         friendsList.push(friendName.value)
         friendName.value = ''
+        showFriendsList(friendName.value)
     }else{
         alert('Preencha o campo de nome!')
+    }
+    
+}
+
+function showFriendsList(){
+    let list = document.getElementById('friendList')
+    let elementList = document.createElement('li')
+
+    for(index=0;index<friendsList.length;index++){
+        elementList.innerHTML = `${friendsList[index]}`
+        list.appendChild(elementList)
     }
     
 }
