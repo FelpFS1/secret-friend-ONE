6bc16c20fe12cc084cf275d2b93d8e4ffeb30989
FelpFS1 <phff800@gmail.com>
2025-01-16 16:06:55 -0300

    feat: add feature - impossible to add twice friend name

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
-let friendsList = []
+let friendsList = [];
 
 function addFriend(){
-    let friendName = document.getElementById('friend')
-    if(friendName.value){
-        friendsList.push(friendName.value)
-        friendName.value = ''
-        showFriendsList(friendName.value)
+    let friendName = document.getElementById('friend');
+    let friendNameIsIncluded = friendsList.includes(friendName.value);
+    if(friendName.value && !friendNameIsIncluded){
+        friendsList.push(friendName.value);
+        friendName.value = '';
+        showFriendsList(friendName.value);
     }else{
-        alert('Preencha o campo de nome!')
-    }
+        let alertElement = document.getElementById('alert');
+        let alertMessage = friendNameIsIncluded ? 'Nome duplicado, adicione outro!' :  'Preencha o campo de nome!';
+        alertElement.innerText = alertMessage;
+    };
     
-}
+};
 
 function showFriendsList(){
-    let list = document.getElementById('friendList')
-    let elementList = document.createElement('li')
+    let list = document.getElementById('friendList');
+    let elementList = document.createElement('li');
 
     for(index=0;index<friendsList.length;index++){
-        elementList.innerHTML = `${friendsList[index]}`
-        list.appendChild(elementList)
-    }
+        elementList.innerHTML = `${friendsList[index]}`;
+        list.appendChild(elementList);
+    };
     
-}
+};
